7626a5bfdfac815bde92700611c67c4a885a92e6
ANJIYUN98 <155033351+ANJIYUN98@users.noreply.github.com>
2024-04-30 12:42:46 +0900

    Add files via upload

--- ex01_Lombok_Di_JunitTests/src/test/java/com/example/app/Di/DiTests.java
+++ ex01_Lombok_Di_JunitTests/src/test/java/com/example/app/Di/DiTests.java
@@ -9,6 +9,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.example.app.domain.dto.PersonComponent;
 import com.example.app.domain.dto.PersonDto;
 
 import lombok.extern.slf4j.Slf4j;
@@ -28,12 +29,16 @@ public class DiTests {
 	@Autowired
 	private PersonDto personBean;
 
+	@Autowired
+	private PersonComponent personComponent;
+	
 	@Test
 	public void beantest() {
 		log.info(personDtoByXml.toString());
 		log.info(personDtoByXml2.toString());
 		log.info(person03.toString());
 		log.info(personBean.toString());
+		log.info(personComponent.toString());
 	}
 	
 	@Autowired
